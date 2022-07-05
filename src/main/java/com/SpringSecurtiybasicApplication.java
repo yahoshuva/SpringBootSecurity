d26d3997b05fdb9fce39a718a6974188756709b5
Yahoshuva <pasupuletiyahoshuva1999@gmail.com>
2022-07-05 11:31:39 -0700

    First commit

---
+++ src/main/java/com/SpringSecurtiybasicApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurtiybasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurtiybasicApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@GetMapping
+	public String sayHello() {
+		return "Hi Welcome to Spring boot";
+	}
+
+}
